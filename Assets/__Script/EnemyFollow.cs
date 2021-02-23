90035b5b31faceb4a84e5685ef02115596387ba8
Roshan Nanthapalan <rnanthap@uwo.ca>
2021-02-23 16:38:47 -0500

    Initial commit

---
+++ Assets/__Script/EnemyFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform player;
+
+    public float health = 25f;
+    public void TakeDamage(float amnt)
+    {
+        health -= amnt;
+        if (health<=0)
+        {
+            print("Enemy Died");
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("Enemy took damage");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+    }
+}
---
+++ Assets/__Script/Player.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody rig;
+
+    public float jumpForce;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public Camera cam;
+    public GameObject hand;
+    public Weapon myWeapon;
+
+    private float attackTimer;
+
+    public float meleeAttackRange;
+    public bool playerInMeleeAttackRange;
+    public LayerMask whatIsEnemy;
+
+
+    private void Start()
+    {
+    }
+
+    void Jump2()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded)
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        Move();   
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump2();
+
+        }
+
+        if (Input.GetMouseButtonUp(0) && attackTimer>=myWeapon.attackCoolDown)
+        {
+
+
+            //DoAttack();
+            DoMeleeAttack();
+        }
+
+    }
+
+    private void DoAttack()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, myWeapon.attackDamage))
+        {
+            if(hit.collider.tag == "Enemy")
+            {
+                EnemyFollow ehealth = hit.collider.GetComponent<EnemyFollow>();
+                ehealth.TakeDamage(myWeapon.attackDamage);
+            }
+        }
+    }
+
+    private void DoMeleeAttack()
+    {
+        playerInMeleeAttackRange = Physics.CheckSphere(transform.position, meleeAttackRange, whatIsEnemy);
+
+        if (playerInMeleeAttackRange)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, myWeapon.attackDamage))
+            {
+                if (hit.collider.tag == "Enemy")
+                {
+                    EnemyFollow ehealth = hit.collider.GetComponent<EnemyFollow>();
+                    ehealth.TakeDamage(myWeapon.attackDamage);
+                }
+            }
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir *= moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+
+    }
+    /*void Jump()
+    {
+        if (CanJump())
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool CanJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 0.5f))
+        {
+            return hit.collider != null;
+        }
+        return false;
+    }*/
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, meleeAttackRange);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+    }
+
+}
