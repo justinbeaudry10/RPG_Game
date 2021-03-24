67c4783263618a629275d3e289937a3a28b14cd4
Roshan Nanthapalan <rnanthap@uwo.ca>
2021-03-24 00:33:35 -0400

    Fixed bugs

--- Assets/__Scripts/ProjectileEnemy.cs
+++ Assets/__Scripts/ProjectileEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile2 : MonoBehaviour
+public class ProjectileEnemy : MonoBehaviour
 {
     public float speed;
 
